d45f6ce5a97a70f7cf3b170386cbdfbd448f4ef2
1815196 <1815196@brunel.ac.uk>
2019-11-02 02:05:22 +0000

    level 4

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -12,20 +12,30 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    String questions[] = new String[]{"If you could go anywhere in the world, where would you go?",
+            "If you were stranded on a desert island, what three things would you want to take with you?",
+            "If you could eat only one food for the rest of your life, what would that be?",
+            "If you won a million dollars, what is the first thing you would buy?",
+            "If you could spend the day with one fictional character, who would it be?",
+            "If you found a magic lantern and a genie gave you three wishes, what would you wish?"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+
+
     public void on_button_click(View view){
          TextView tv = this.findViewById(R.id.textView);
          TextView inputBox = this.findViewById(R.id.input);
          TextView scoreBox = this.findViewById(R.id.scoreNumber);
-
+         try{
          int usrNumber = Integer.parseInt(inputBox.getText().toString());
-         Random r = new Random();
-         int number = r.nextInt(6)+1;
+
+         int number = roll_the_dice(6);
          int score = Integer.parseInt(scoreBox.getText().toString());
 
         if(usrNumber == number){
@@ -38,5 +48,25 @@ public class MainActivity extends AppCompatActivity {
         }
         else
             tv.setText(Integer.toString(number));
+    }
+         catch (Exception e){
+             tv.setText("Bruh");
+         }
+    }
+
+    public void on_ice_click(View view){
+        TextView tv = this.findViewById(R.id.textView);
+        try {
+            tv.setText(questions[roll_the_dice(6)]);
+        }
+        catch (Exception e){
+            tv.setText("");
+        }
+    }
+
+    public int roll_the_dice(int MaxRandNumber){
+        Random r = new Random();
+        int RandNumber = r.nextInt(MaxRandNumber)+1;
+        return RandNumber;
     }
 }
